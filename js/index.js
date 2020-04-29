f1ced90e7f59f1c0aef2a9a197ad99d38dd1cec4
nebocamin <github@tanebi.de>
2020-04-29 15:30:18 +0200

    16:00

--- js/index.js
+++ js/index.js
@@ -41,7 +41,7 @@ $(function () {
         KWker: {
           radius: 10
         },
-        data: [1,18]
+        data: [1,28]
       }, {
         name: 'Maximal',
         color: 'rgba(225,200,0,0.75)',
