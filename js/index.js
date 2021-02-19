0b3ee2ec344ee29d79edae20ddd5d954fb117662
nebocamin <github@tanebi.de>
2021-02-19 01:36:15 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -12,7 +12,7 @@ $(function () {
       }
     },
     xAxis: {
-      categories: ['Sprint 1','Sprint 2','Sprint 3','Sprint 4','Sprint 5','Sprint 6','Sprint 7','Sprint 8']
+      categories: ['Start','Sprint 1','Sprint 2','Sprint 3','Sprint 4','Sprint 5','Sprint 6','Sprint 7','Sprint 8']
     },
     yAxis: {
       min: 0,
