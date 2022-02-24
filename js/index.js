03694bd5e388d268afb85e1eaf716a2e705c8c53
nebocamin <github@tanebi.de>
2022-02-24 13:25:04 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -34,14 +34,14 @@ $(function () {
         name: 'Ideal',
         color: 'rgba(0,255,0,0.25)',
         lineWidth: 1,
-        data: [0,37,62,95,119,148]
+        data: [0,37,62,95,119]
       }, {
         name: 'Actual',
         color: 'rgba(0,120,200,0.75)',
         KWker: {
           radius: 10
         },
-        data: [0,37,74,111]
+        data: [0,37,74,111,148]
       }, {
         name: 'Maximal',
         color: 'rgba(225,200,0,0.75)',
